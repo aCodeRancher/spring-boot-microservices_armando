4eb09632d35bc7c56dffab6fef2abc5e2b5ace71
helen <hhma@alumni.cpp.edu>
2022-06-19 22:34:39 -0400

    Use @EnableWebSecurity and return SecurityFilterChain instead of the deprecated WebSecurityConfig

--- mssc-beer-inventory/src/main/java/sfg/beer/inventory/service/config/WebSecurityConfig.java
+++ mssc-beer-inventory/src/main/java/sfg/beer/inventory/service/config/WebSecurityConfig.java
@@ -1,19 +1,21 @@
 package sfg.beer.inventory.service.config;
 
-
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
+        return http.build();
     }
 }
